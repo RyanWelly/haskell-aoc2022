4443e865d2b17d70ebe6605fb81278e60dfe4340
Ryan Winter <100173972+RyanWinter1@users.noreply.github.com>
2023-06-27 13:57:32 +1200

    Create day4a.hs

---
+++ day4a.hs
@@ -0,0 +1,29 @@
+main :: IO ()
+main = do
+    input <- readFile "day4.input"
+    putStrLn . show $ solution input
+
+type Range = (Int, Int)
+
+parseLine :: String -> (Range, Range)
+parseLine s = listPair (map parseRange $ splitOn s ',')
+
+parseRange :: String -> Range
+parseRange s = listPair $ map read $ splitOn s '-'
+
+--convenience function for converting lists of length two into a 2-tuple
+listPair :: [a] -> (a, a)
+listPair [x,y] = (x,y)
+listPair xs = error "Input is not length two"
+
+splitOn :: String -> Char -> [String]
+splitOn [] _ = []
+splitOn s c = chunk : (splitOn (drop 1 rest) c)
+            where 
+                (chunk, rest) = span (/= c) s
+
+fullyContains :: (Range, Range) -> Bool
+fullyContains ((x1, x2), (y1, y2)) = (x1 <= y1 && x2 >= y2) || (y1 <= x1 && y2 >= x2) 
+
+solution :: String -> Int
+solution s = length . filter (==True) . map fullyContains . map parseLine $ lines s 
